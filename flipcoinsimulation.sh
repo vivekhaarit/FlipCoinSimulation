8b1d444638dc824a0234094c6a50391eba0138b3
vivek kumar <vivekhaarit@gmail.com>
2020-03-25 19:15:33 +0530

    [vivek]Added:doublet combination created and % of results evaluated

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -5,17 +5,25 @@ echo ""
 echo "this will show you winning percentage of head or tail"
 echo "     combinaton in a singlet doublet or triplet"
  
-declare -A toss
+#declare -A tossresult
 
-for ((i=1;i<=10;i++))
-do
-	if [ $(($RANDOM%10+1)) -le 5 ]
+for ((i=1;i<=10;i++))do	
+	doublet=$(($RANDOM%100+1))
+	if [ $doublet -le 25 ]
 	then 
-		toss="H"		
-		tossresult[i]=$toss		
+		toss="HH"		
+		tossresult[$i]=$toss	
+	elif [ $doublet -gt 25 ] && [ $doublet -le 50 ]
+	then
+		toss="HT"		
+		tossresult[$i]=$toss
+	elif [ $doublet -gt 50 ] && [ $doublet -le 75 ]
+	then		
+		toss="TH"		
+		tossresult[$i]=$toss
 	else
-		toss="T"		
-		tossresult[i]=$toss	
+		toss="TT"		
+		tossresult[$i]=$toss	
 	fi
 done
 
@@ -25,24 +33,32 @@ echo ${tossresult[@]}
 
 #calculating percentage of flips
 n=${#tossresult[@]}
-headcount=0;
-tailcount=0;
+HHcount=0;
+HTcount=0;
+THcount=0;
+TTcount=0;
+TH
 for ((i=1;i<=$n;i++))
 do
-	if [ ${tossresult[i]} == "H" ]
+	if [ ${tossresult[i]} == "HH" ] 
 	then
-		((headcount++))
+		((HHcount++))
+	elif [ ${tossresult[i]} == "HT" ] 
+	then
+		((HTcount++))
+	elif [ ${tossresult[i]} == "TH" ] 
+	then
+		((THcount++))
 	else
-		((tailcount++))
+		((TTcount++))
 	fi
 done
 
-echo "Head %: "
-echo "scale=4;$(($(($headcount*100))/${#tossresult[@]}))" | bc
-echo "Tail %: "
-echo "scale=4;$(($(($tailcount*100))/${#tossresult[@]}))" | bc
-
-
+#calculating percentage:
+echo "HH%: "$(($(($HHcount*100))/${#tossresult[@]}))
+echo "HT%: "$(($(($HTcount*100))/${#tossresult[@]}))
+echo "TH%: "$(($(($THcount*100))/${#tossresult[@]}))
+echo "TT%: "$(($(($TTcount*100))/${#tossresult[@]}))
 
 
 
