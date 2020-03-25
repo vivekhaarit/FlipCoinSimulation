c33196a087a806ecaefdf7f76d80e56c056cc1e2
vivek kumar <vivekhaarit@gmail.com>
2020-03-25 14:07:52 +0530

    [vivek]Added:displayed head/tail using random fn

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -5,10 +5,12 @@ echo ""
 echo "this will show you winning percentage of head or tail"
 echo "     combinaton in a singlet doublet or triplet"
 
-
-
-
-
+if [ $(($RANDOM%10+1)) -le 5 ]
+then 
+	echo "head"
+else
+	echo "tail"
+fi
 
 
 
