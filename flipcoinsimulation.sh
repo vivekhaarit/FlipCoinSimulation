8e0b4d015e3498105f80a40bfeda9e164950d299
vivek kumar <vivekhaarit@gmail.com>
2020-03-25 21:48:31 +0530

    [vivek]Added:percentage of triplets calculated in sample space of 100 flips

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -7,22 +7,38 @@ echo "     combinaton in a singlet doublet or triplet"
  
 #declare -A tossresult
 
-for ((i=1;i<=10;i++))do	
-	doublet=$(($RANDOM%100+1))
-	if [ $doublet -le 25 ]
+for ((i=1;i<=100;i++))do	
+	doublet=$(($RANDOM%1000+1))
+	if [ $doublet -le 125 ]
 	then 
-		toss="HH"		
+		toss="HHH"		
 		tossresult[$i]=$toss	
-	elif [ $doublet -gt 25 ] && [ $doublet -le 50 ]
+	elif [ $doublet -gt 125 ] && [ $doublet -le 250 ]
 	then
-		toss="HT"		
+		toss="HHT"		
 		tossresult[$i]=$toss
-	elif [ $doublet -gt 50 ] && [ $doublet -le 75 ]
+	elif [ $doublet -gt 250 ] && [ $doublet -le 375 ]
 	then		
-		toss="TH"		
+		toss="HTH"		
+		tossresult[$i]=$toss
+	elif [ $doublet -gt 375 ] && [ $doublet -le 500 ]
+	then		
+		toss="HTT"		
+		tossresult[$i]=$toss
+	elif [ $doublet -gt 500 ] && [ $doublet -le 625 ]
+	then		
+		toss="THH"		
+		tossresult[$i]=$toss
+	elif [ $doublet -gt 625 ] && [ $doublet -le 750 ]
+	then		
+		toss="THT"		
+		tossresult[$i]=$toss
+	elif [ $doublet -gt 750 ] && [ $doublet -le 875 ]
+	then		
+		toss="TTH"		
 		tossresult[$i]=$toss
 	else
-		toss="TT"		
+		toss="TTT"		
 		tossresult[$i]=$toss	
 	fi
 done
@@ -33,33 +49,51 @@ echo ${tossresult[@]}
 
 #calculating percentage of flips
 n=${#tossresult[@]}
-HHcount=0;
-HTcount=0;
-THcount=0;
-TTcount=0;
-TH
+HHHcount=0;
+HHTcount=0;
+HTHcount=0;
+HTTcount=0;
+THHcount=0;
+THTcount=0;
+TTHcount=0;
+TTTcount=0;
 for ((i=1;i<=$n;i++))
 do
-	if [ ${tossresult[i]} == "HH" ] 
+	if [ ${tossresult[i]} == "HHH" ] 
 	then
-		((HHcount++))
-	elif [ ${tossresult[i]} == "HT" ] 
+		((HHHcount++))
+	elif [ ${tossresult[i]} == "HHT" ] 
 	then
-		((HTcount++))
-	elif [ ${tossresult[i]} == "TH" ] 
+		((HHTcount++))
+	elif [ ${tossresult[i]} == "HTH" ] 
 	then
-		((THcount++))
+		((HTHcount++))
+	elif [ ${tossresult[i]} == "HTT" ] 
+	then
+		((HTTcount++))
+	elif [ ${tossresult[i]} == "THH" ] 
+	then
+		((THHcount++))
+	elif [ ${tossresult[i]} == "THT" ] 
+	then
+		((THTcount++))
+	elif [ ${tossresult[i]} == "TTH" ] 
+	then
+		((TTHcount++))
 	else
-		((TTcount++))
+		((TTTcount++))
 	fi
 done
 
 #calculating percentage:
-echo "HH%: "$(($(($HHcount*100))/${#tossresult[@]}))
-echo "HT%: "$(($(($HTcount*100))/${#tossresult[@]}))
-echo "TH%: "$(($(($THcount*100))/${#tossresult[@]}))
-echo "TT%: "$(($(($TTcount*100))/${#tossresult[@]}))
-
+echo "HHH%: "$(($(($HHHcount*100))/${#tossresult[@]}))
+echo "HHT%: "$(($(($HHTcount*100))/${#tossresult[@]}))
+echo "HTH%: "$(($(($HTHcount*100))/${#tossresult[@]}))
+echo "HTT%: "$(($(($HTTcount*100))/${#tossresult[@]}))
+echo "THH%: "$(($(($THHcount*100))/${#tossresult[@]}))
+echo "THT%: "$(($(($THTcount*100))/${#tossresult[@]}))
+echo "TTH%: "$(($(($TTHcount*100))/${#tossresult[@]}))
+echo "TTT%: "$(($(($TTTcount*100))/${#tossresult[@]}))
 
 
 
