24d93199f95545d5d7f4fd69a8f71a811abf9522
vivek kumar <vivekhaarit@gmail.com>
2020-03-25 13:44:17 +0530

    [vivek]Added:greeted user

---
+++ flipcoinsimulation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "*******welcome to flip coin simulation program*******"
+echo ""
+echo "this will show you winning percentage of head or tail"
+echo "     combinaton in a singlet doublet or triplet"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
